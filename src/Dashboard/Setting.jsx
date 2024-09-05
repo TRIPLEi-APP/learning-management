0cefe9262817499f88c59f72eb8e96356368febe
rashaduldev <rashadul.dev@gmail.com>
2024-09-05 17:22:14 +0600

    Replace all fake data to Real Data. by Rashadul

--- src/Dashboard/Setting.jsx
+++ src/Dashboard/Setting.jsx
@@ -3,7 +3,13 @@
 const Setting = () => {
     return (
         <div>
-            Settings
+               {/* Button group */}
+      <div className="flex items-center mb-10">
+          <button className="px-4 py-2 bg-[#A9BF1C] text-white mr-4">Password Setting</button>
+          <button className="px-4 py-2 bg-[#F4F6F8] text-[#1D2026] hover:bg-[#A9BF1C] hover:text-white mr-4">Social Media</button>
+          <button className="px-4 py-2 bg-[#F4F6F8] text-[#1D2026] hover:bg-[#A9BF1C] hover:text-white mr-4">Linked Accounts</button> 
+          <button className="px-4 py-2 bg-[#F4F6F8] text-[#1D2026] hover:bg-[#A9BF1C] hover:text-white mr-4">Notification</button>
+      </div>
         </div>
     );
 };
